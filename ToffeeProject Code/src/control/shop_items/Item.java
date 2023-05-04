2d5fb3f08148f3b29c83535ecbad53aeefd17184
MayaZayn <mayozain5@gmail.com>
2023-05-04 20:45:59 +0300

    Add Item Class and ItemStatus Enum

--- ToffeeProject Code/src/control/shop_items/Item.java
+++ ToffeeProject Code/src/control/shop_items/Item.java
@@ -1,73 +1,82 @@
 package control.shop_items;
-
 public class Item {
     private String name;
     private String description;
     private String image;
     private String brand;
     private String unitType;
-    private Double price;
     private ItemStatus itemStatus;
+    private double price;
 
-    public enum ItemStatus{
-        OnSALE,
-        OUTofSTOCK,
-        NOTonSALE;
+    public Item(String name, String description, String image, String brand,
+                String unitType, ItemStatus itemStatus, double price) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.brand = brand;
+        this.unitType = unitType;
+        this.itemStatus = itemStatus;
+        this.price = price;
+    }
+
+    public void applyDiscount(double discountAmount) {
+        itemStatus = ItemStatus.onSale;
+        price *= 100 - discountAmount;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getImage() {
         return image;
     }
 
-    public void setImage(String image) {
-        this.image = image;
-    }
-
     public String getBrand() {
         return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
     }
 
     public String getUnitType() {
         return unitType;
     }
 
-    public void setUnitType(String unitType) {
-        this.unitType = unitType;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
     public ItemStatus getItemStatus() {
         return itemStatus;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setUnitType(String unitType) {
+        this.unitType = unitType;
+    }
+
     public void setItemStatus(ItemStatus itemStatus) {
         this.itemStatus = itemStatus;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
