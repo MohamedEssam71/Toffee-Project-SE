3bdb994d73d90d56d79bf82aa2b1ea13a56833e8
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-05-08 21:57:20 +0300

    Add comments for JavaDoc in OTPManager.java

--- ToffeeProject Code/src/control/Authentication/OTPManager.java
+++ ToffeeProject Code/src/control/Authentication/OTPManager.java
@@ -4,8 +4,18 @@ import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This is the OTP manager class; it is responsible for
+ * generating OTP, sending OTP, and verifying the OTP.
+ *
+ * @author Maya Ayman
+ */
 public class OTPManager {
     private String OTP;
+    /**
+     * This method generates an OTP from a mix of
+     * uppercase and lowercase letters and numbers
+     */
     public void generateOTP() {
         String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String Small_chars = "abcdefghijklmnopqrstuvwxyz";
@@ -20,9 +30,17 @@ public class OTPManager {
 
         OTP = password.toString();
     }
+    /**
+     * This method displays the OTP on the console.
+     */
     public void sendOTP(String email) {
         System.out.println(OTP);
     }
+    /**
+     * This method verifies that the OTP entered by
+     * the user matches the one sent.
+     * @return boolean
+     */
     public boolean verifyOTP() {
         System.out.println("Please Enter the OTP:");
         Scanner scanner = new Scanner(System.in);
