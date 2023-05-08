4e636354fa5fc8d2129732205496757b20cc96a9
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-05-08 21:56:44 +0300

    Add comments for JavaDoc in DeliveryPay.java

--- ToffeeProject Code/src/control/DeliveryPay.java
+++ ToffeeProject Code/src/control/DeliveryPay.java
@@ -1,7 +1,19 @@
 package control;
 import actors.Attachtments.Order;
 
+/**
+ * This class represents the pay on
+ * delivery option.
+ *
+ * @author Maya Ayman
+ */
 public class DeliveryPay implements PaymentMethod {
+    /**
+     * This method allows the user to
+     * make a payment.
+     * @param order
+     * @return boolean This shows whether the payment was successful.
+     */
     @Override
     public boolean makePayment(Order order) {
         return (order.getTotalPrice() <= 2000);
