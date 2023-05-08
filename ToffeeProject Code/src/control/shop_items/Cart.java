84388ff955975aa182451cb94c68af56e55572f2
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-05-08 21:55:38 +0300

    Add comments for JavaDoc in Cart.java

--- ToffeeProject Code/src/control/shop_items/Cart.java
+++ ToffeeProject Code/src/control/shop_items/Cart.java
@@ -3,12 +3,31 @@ package control.shop_items;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class represents an online cart.
+ * It has a list of items to be purchased by a
+ * certain user and allows user to add/remove
+ * items to/from the cart.
+ *
+ * @author Maya Ayman
+ * @author Mohamed Essam
+ */
 public class Cart {
     private Map<Item, Integer> itemsList = new HashMap<>();
 
+    /**
+     * This method allows the user to add an
+     * item to his cart.
+     * @param item
+     */
     public void addToCart(Item item){
         itemsList.put(item, itemsList.getOrDefault(item, 0) + 1);
     }
+    /**
+     * This method allows the user to remove an
+     * item from his cart.
+     * @param item
+     */
     public void removeFromCart(Item item){
         itemsList.remove(item);
     }
@@ -16,5 +35,4 @@ public class Cart {
     public Map<Item, Integer> getItemsList() {
         return itemsList;
     }
-    public int getQty(Item item) { return itemsList.get(item); }
 }
