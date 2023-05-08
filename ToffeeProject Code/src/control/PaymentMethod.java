aa4729d397e5395cca4103d7b57d303150157aae
Maya Zain El-Din <102183224+MayaZayn@users.noreply.github.com>
2023-05-08 21:56:18 +0300

    Add comments for JavaDoc in PaymentMethod.java

--- ToffeeProject Code/src/control/PaymentMethod.java
+++ ToffeeProject Code/src/control/PaymentMethod.java
@@ -2,6 +2,11 @@ package control;
 
 import actors.Attachtments.Order;
 
+/**
+ * This interface defines one method
+ * that allows the user to make a payment
+ * through different available payment methods
+ */
 public interface PaymentMethod {
     boolean makePayment(Order order);
 }
