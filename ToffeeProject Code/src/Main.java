b53a26ebce4d2702a881ae2e49902d3e63354cc6
Mohamed Essam <MEssam.SDE@gmail.com>
2023-05-10 03:00:34 +0300

    Update Main.java

--- ToffeeProject Code/src/Main.java
+++ ToffeeProject Code/src/Main.java
@@ -1,6 +1,7 @@
 import gui.UserInterface;
 
 /**
+ * <H2>Toffee Project</H2>
  * Main Class of Toffee Project System.
  * This System has a main menu to log in,register, and forgetPassword.<br>
  * It has a catalog page, item page, check out page, order receipt, and address page.<br>
